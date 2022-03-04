a629c07d6d926e130dd528c9ba820b28acf98633
Brandon Miller <bmiller1881@gmail.com>
2022-03-03 22:57:28 -0800

    initial commit for test branch

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //jshint esversion:6
-require("dotenv").config();
 
+//load JS modules
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -12,12 +13,13 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const FacebookStrategy = require("passport-facebook").Strategy;
 const findOrCreate = require("mongoose-findorcreate");
 
+//create the express application
 const app = express();
 
+//setup application middleware
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(
   session({
     secret: "Our little secret.",
@@ -32,12 +34,17 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+//set variable to URL of environment path
+const setDeploymentEnv = "https://secrets-project-bmiller-test.herokuapp.com";
+
+//connect to MongoDB
 mongoose.connect(
   "mongodb+srv://bmiller1881:" +
     process.env.CLIENT_SECRET_MONGODB +
     "@cluster0.clksm.mongodb.net/userDB?retryWrites=true&w=majority"
 );
 
+//create schhema
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
@@ -46,18 +53,17 @@ const userSchema = new mongoose.Schema({
   secret: [String],
 });
 
-// Deployment Environment
-const localDeploymentEnv = "http://localhost:3000";
-const herokuDeploymentEnv = "https://secrets-project-bmiller.herokuapp.com";
-const setDeploymentEnv = herokuDeploymentEnv;
-
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
 
-const User = new mongoose.model("User", userSchema);
+//create collection
+const User = new mongoose.model("User_test", userSchema);
 
+//setup passport.js to use userDB
 passport.use(User.createStrategy());
 
+//serialize and deserialize user session
+//explanation:"https://stackoverflow.com/questions/27637609/understanding-passport-serialize-deserialize"
 passport.serializeUser(function (user, done) {
   done(null, user.id);
 });
@@ -68,14 +74,16 @@ passport.deserializeUser(function (id, done) {
   });
 });
 
+//Google strategy setup to enable login with Google Account
+//documentation: "https://www.passportjs.org/packages/passport-google-oauth20/"
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
       callbackURL: setDeploymentEnv + "/auth/google/secrets",
-      // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
     },
+    //API: "https://console.cloud.google.com/apis/credentials/consent/"
     function (accessToken, refreshToken, profile, cb) {
       User.findOrCreate({ googleId: profile.id }, function (err, user) {
         return cb(err, user);
@@ -84,6 +92,8 @@ passport.use(
   )
 );
 
+//Facebook strategy setup to enable login with Google Account
+//documentation: "https://www.passportjs.org/packages/passport-facebook/"
 passport.use(
   new FacebookStrategy(
     {
@@ -91,6 +101,7 @@ passport.use(
       clientSecret: process.env.CLIENT_SECRET_FB,
       callbackURL: setDeploymentEnv + "/auth/facebook/secrets",
     },
+    //API: "https://developers.facebook.com/apps/"
     function (accessToken, refreshToken, profile, cb) {
       console.log(profile);
       User.findOrCreate({ facebookId: profile.id }, function (err, user) {
@@ -104,9 +115,10 @@ app.route("/").get(function (req, res) {
   res.render("home");
 });
 
+//authenticate using google account
 app
   .route("/auth/google")
-  .get(passport.authenticate("google", { scope: ["profile"] }));
+  .get(passport.authenticate("google", { scope: ["email", "profile"] }));
 
 app
   .route("/auth/google/secrets")
@@ -118,6 +130,7 @@ app
     }
   );
 
+//authenticate using facebook account
 app.route("/auth/facebook").get(passport.authenticate("facebook"));
 
 app
@@ -130,6 +143,7 @@ app
     }
   );
 
+//authenticate using local Passport strategy
 app
   .route("/login")
   .get(function (req, res) {
@@ -152,6 +166,7 @@ app
     });
   });
 
+//secrets page can only be accessed when logged in
 app.route("/secrets").get(function (req, res) {
   User.find({ secret: { $ne: null } }, function (err, foundUsers) {
     if (err) {
@@ -218,5 +233,5 @@ app.route("/logout").get(function (req, res) {
 });
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log("Server started on port 3000.");
+  console.log("Server started.");
 });
